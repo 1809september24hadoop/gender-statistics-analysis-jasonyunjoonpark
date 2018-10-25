c1942b9fb482b0cfdddeeb0aacde39d57c9b31e9
Jason Park <park.jason@outlook.com>
2018-10-24 18:49:16 -0700

    Project progress.

--- ProjectOne/src/main/java/com/revature/reduce/ReducerThree.java
+++ ProjectOne/src/main/java/com/revature/reduce/ReducerThree.java
@@ -5,31 +5,28 @@ import java.io.IOException;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class ReducerTwo extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+public class ReducerThree extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 	
 	
 	
 	@Override
 	public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 		
-		double sumOfDifferences = 0.0;	
-		double previousValue = 0.0;
+		double difference = 0.0;
+		double currentValue = values.iterator().next().get();  
 		double nextValue = 0.0;
-		int counter = 0;
-		
-		if(values.iterator().hasNext()) {
-			previousValue = values.iterator().next().get();
-		}
+		double average = 0.0;
 		
 		while(values.iterator().hasNext()) {
 			nextValue = values.iterator().next().get();
-			sumOfDifferences += (previousValue - nextValue);	
-			previousValue = nextValue;
-			counter++;
+			difference = currentValue - nextValue;	
 		}
 		
-		context.write(key, new DoubleWritable(sumOfDifferences/counter));
+		average = difference/nextValue;
+		
+		context.write(key, new DoubleWritable(average));
 	
 	}
 }
